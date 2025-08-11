6261a75d2440c12ab61194f55262976783ef9525
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2025-08-12 01:41:37 +0530

    Update Footer.tsx

--- src/app/Component/Footer.tsx
+++ src/app/Component/Footer.tsx
@@ -117,7 +117,7 @@ const Footer = () => {
           </p>
           <button className="bg-blue-500 mt-5 text-white font-bold p-3 rounded-md transition transform hover:scale-105 active:scale-95 shadow-lg">
             <a
-              href="https://drive.google.com/file/d/1W9adFqPhh_j04wSseIMCBfBwY8QTTuPQ/view?usp=sharing"
+              href="https://drive.google.com/file/d/1M1ejjM8TO-NwHJGf5bfDu0JDCt4O5IIq/view?usp=drivesdk"
               target="_blank"
               rel="noopener noreferrer"
             >
